2a2fed3861ddfb1dfb8557603675b339247792e5
unknown <lizhen232002@gmail.com>
2020-03-13 22:42:02 +0300

    feat: enter code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const result = [];
+    for (const char of str) {
+        if (result[result.length - 1] == char) {
+            result.pop();
+        } else {
+            const pair = bracketsConfig.find(pair => pair[0] === char);
+            if (pair) result.push(pair[1]);
+            else return false;
+        }
+    }
+    return !result.length;
+};
